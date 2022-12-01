f10e208e59092ca05b7a68d049f5afd3bc6f8c77
Ben Clark <beclark@blizzard.com>
2022-12-01 13:50:18 -0800

    Initial commit, day 1 solutions

---
+++ 01_Calories.hs
@@ -0,0 +1,26 @@
+import System.IO
+import System.Environment
+import Data.List.Split
+import Data.List
+
+-- Day 1: Calorie Counting
+-- Part 1: Find the most calories carried by a single elf
+-- Part 2: Find the total calories carried by the top three elves
+
+parseIntLists :: [String] -> [[Int]]
+parseIntLists values = map (map (read::String->Int)) (splitOn [""] values)
+
+partTwo :: [String] -> Int
+partTwo values = sum (take 3 (reverse (sort (map (sum) (parseIntLists values)))))
+
+partOne :: [String] -> Int
+partOne values = maximum (map (sum) (parseIntLists values))
+
+main :: IO ()
+main = do
+    args <- getArgs
+    content <- readFile (args !! 0)
+    let outputOne = partOne (lines content)
+    print outputOne
+    let outputTwo = partTwo (lines content)
+    print outputTwo
