a8511920951ea100c1fe653e74bceb871faff93f
Ben Clark <benfclark@urog.co.uk>
2022-12-28 10:07:58 -0800

    Update 07_DeviceSpace.hs

--- 07_DeviceSpace.hs
+++ 07_DeviceSpace.hs
@@ -1,14 +1,28 @@
 import System.IO
 import System.Environment
 import Data.List.Split
-import Data.List
+import Data.List as List
 import Data.Map as Map
+import Data.HashSet as HashSet
 
--- Day 7: No Space Left On Device
--- Part 1: Find sum of total sizes of directories
--- Part 2: 
 
 data FileSys = File {fileName::String, fileSize::Int} | Directory {dirName::String, dirContents::(Map String FileSys)} deriving (Show, Eq)
+
+
+addToDir :: FileSys -> FileSys -> FileSys
+addToDir (Directory dName dCont) file = Directory{dirName = dName, dirContents = Map.insert (fileName file) file dCont}
+addToDir f _ = f
+
+getDirectoryLines :: [String] -> [String]
+getDirectoryLines (l:lines) = l:(getDirectoryLines' lines 1)
+    where 
+        getDirectoryLines' [] _ = []
+        getDirectoryLines' _ 0 = []
+        getDirectoryLines' (l:lines) n
+            | isPrefixOf "$ cd .." l = l:(getDirectoryLines' lines (n - 1))
+            | isPrefixOf "$ cd" l = l:(getDirectoryLines' lines (n + 1))
+            | isPrefixOf "$ ls" l = getDirectoryLines' lines n
+            | otherwise = l:(getDirectoryLines' lines n)
 
 parseFileSystemLine :: String -> FileSys
 parseFileSystemLine input
@@ -17,10 +31,30 @@ parseFileSystemLine input
     where
         splitInput = splitOn " " input
 
-
 parseFileSystem :: [String] -> FileSys -> FileSys
-parseFileSystem list dir
-    | (head list) == 
+parseFileSystem [] dir = dir
+parseFileSystem (l:list) dir
+    | isPrefixOf "$ cd .." l = dir
+    | isPrefixOf "$ cd" l = parseFileSystem (List.drop (length childLines) list) (Directory (dirName dir) (Map.adjust (\x -> (parseFileSystem childLines x)) targetName (dirContents dir)))
+    | isPrefixOf "$ ls" l = parseFileSystem list dir
+    | otherwise = parseFileSystem list (addToDir parsedLine dir)
+        where
+            parsedLine = parseFileSystemLine l
+            childLines = getDirectoryLines list
+            targetName = List.drop 5 l
+
+
+sumFileSizes :: [String] -> Int
+sumFileSizes lines = sumFileSizes' lines [] HashSet.empty
+    where
+        sumFileSizes' (l:lines) stack set
+        | isPrefixOf "$ cd .." l = sumFileSizes' total lines (init stack) set
+        | isPrefixOf "$ cd " l = sumFileSizes' total lines (dirname:stack) (HashSet.insert dirname set) 
+        | isPrefixOf "dir" l = sumFileSizes' total lines stack set
+        |  = sumFileSizes' total lines stack set
+        | otherwise = 0
+        dirname = drop 5 l
+        fileSize = read (head (splitOn " " l))::Int
 
 partOne :: [String] -> Int
 partOne [] = 0
